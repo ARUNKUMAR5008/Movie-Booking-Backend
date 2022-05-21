2f10d91ab727f4c8830e05d1b29c72dac3606375
ARUNKUMAR5008 <marun64@gmail.com>
2022-05-21 14:43:31 +0530

    check point 5

--- server.js
+++ server.js
@@ -1,6 +1,21 @@
 const http = require("http");
 const PORT = 9000;
 
+const db = require("./app/models");
+db.mongoose
+  .connect(db.url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    console.log("Connected to the database!");
+    
+  })
+  .catch(err => {
+    console.log("Cannot connect to the database!", err);
+    process.exit();
+  });
+
 const ROUTE_RESPONSE_MAP = {
   "/movies": "All Movies Data in JSON format from Mongo DB",
   "/genres": "All Genres Data in JSON format from Mongo DB",
