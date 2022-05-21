609051dd361cb4ffe8dbfb40f4dfb82d5362adae
ARUNKUMAR5008 <marun64@gmail.com>
2022-05-21 16:29:38 +0530

    check point 6

--- server.js
+++ server.js
@@ -1,33 +1,8 @@
 const http = require("http");
-const PORT = 9000;
-
-const db = require("./app/models");
-db.mongoose
-  .connect(db.url, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  })
-  .then(() => {
-    console.log("Connected to the database!");
-    
-  })
-  .catch(err => {
-    console.log("Cannot connect to the database!", err);
-    process.exit();
-  });
-
-const ROUTE_RESPONSE_MAP = {
-  "/movies": "All Movies Data in JSON format from Mongo DB",
-  "/genres": "All Genres Data in JSON format from Mongo DB",
-  "/artists": "All Artists Data in JSON format from Mongo DB",
-};
-
-const server = http.createServer((req, res) => {
-  if (ROUTE_RESPONSE_MAP[req.url]) {
-    res.end(ROUTE_RESPONSE_MAP[req.url]);
-  }
-});
+const app = require("./app");
+const server = http.createServer(app);
+const PORT = process.env.PORT || 3000;
 
 server.listen(PORT, () => {
-  console.log(`server listening on port 9000`);
-});
+    console.log(`server listening on port ${PORT}`);
+  });
